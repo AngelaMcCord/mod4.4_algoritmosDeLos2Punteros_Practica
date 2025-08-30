da1f62312e139733934c295a5b7c3a4553ea13d3
AngelaMc <angela.corderoconde@gmail.com>
2025-08-30 19:27:56 -0400

    Agrega función para realizar el mismo proceso con fuerza bruta

--- main.js
+++ main.js
@@ -23,3 +23,21 @@ function encontrarPareja(arr) {
 
 console.log(encontrarPareja(invitados));
 // Resultado: ["Carlos", "Cecilia"]
+
+
+//Ejercicio de fuerza bruta
+function fuerzaBruta(arr) {
+  for (let i=0;i<arr.length;i++) {
+    for (let j=i+1; j<arr.length;j++) {
+      x = arr[i].charAt(0);
+      y = arr[j].charAt(0);
+        
+      if (x==y){
+        return `Los invitados ${arr[i]} y ${arr[j]} se sentarán al lado.`;
+      }
+    } 
+  }
+  return "No se encuentran datos";
+}
+
+console.log(fuerzaBruta(invitados));
